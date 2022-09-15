7f040450e941dfb6ab86b57a5edc18eccfd54717
sholehmuhammad3 <sholeh.sholeh.muhammad@gmail.com>
2022-09-15 10:43:37 +0700

    bc-telegram

---
+++ assets/_...all_.68b897d1.js
@@ -0,0 +1,1 @@
+import{d as t,o,c as a,t as s,e as c,f as e}from"./index.e4284f54.js";const r=t({__name:"[...all]",setup(f){const{t:n}=useI18n();return(l,p)=>(o(),a("div",null,s(c(n)("not-found")),1))}});typeof e=="function"&&e(r);export{r as default};
